2496320dbbb44fe7dc171587b5ffa972c3c52d33
psychobunny <psycho.bunny@hotmail.com>
2015-02-20 13:40:04 -0500

    moved defn's into definitions.js

--- library.js
+++ library.js
@@ -1,100 +1,24 @@
 "use strict";
 
-var plugin = {};
+var plugin = {},
+	definitions = require('./lib/definitions'),
+	rewards = module.parent.require('./rewards'),
+	user = module.parent.require('./user');
 
-plugin.getConditions = function(conditions, callback) {
-	conditions = conditions.concat([
-		{
-			"name": "Reputation",
-			"condition": "core:user.reputation"
-		},
-		{
-			"name": "Post Count",
-			"condition": "core:user.postcount"
-		},
-		{
-			"name": "Last Logged in Time",
-			"condition": "core:user.lastonline"
-		}
-	]);
+plugin.getConditions = definitions.getConditions;
+plugin.getConditionals = definitions.getConditionals;
+plugin.getRewards = definitions.getRewards;
 
-	callback(false, conditions);
+
+
+
+plugin.onUpvote = function(data) {
+	var uid = data.uid;
+
+
+	rewards.checkConditionAndRewardUser(uid, 'core:user.reputation', function(callback) {
+		user.getUserField(uid, 'reputation', callback);
+	});
 };
-
-plugin.getConditionals = function(conditionals, callback) {
-	conditionals = conditionals.concat([
-		{
-			"name": ">",
-			"conditional": "greaterthan"
-		},
-		{
-			"name": ">=",
-			"conditional": "greaterorequalthan"
-		},
-		{
-			"name": "<",
-			"conditional": "lesserthan"
-		},
-		{
-			"name": "<=",
-			"conditional": "lesserorequalthan"
-		},
-		{
-			"name": "string:",
-			"conditional": "string"
-		}
-	]);
-
-	callback(false, conditionals);
-};
-
-plugin.getRewards = function(rewards, callback) {
-	rewards = rewards.concat([
-		{
-			"rid": "core:add-to-group",
-			"name": "Add to Group",
-			"inputs": [
-				{
-					"type": "select",
-					"name": "groupname",
-					"label": "Group Name:",
-					"values": [
-						{
-							"name": "Group 1",
-							"value": "group1"
-						},
-						{
-							"name": "Group 2",
-							"value": "group2"
-						},
-						{
-							"name": "Group 3",
-							"value": "group3"
-						}
-					],
-				}
-			]
-		},
-		{
-			"rid": "core:alert-user",
-			"name": "Send alert message",
-			"inputs": [
-				{
-					"type": "text",
-					"name": "title",
-					"label": "Title:"
-				},
-				{
-					"type": "text",
-					"name": "message",
-					"label": "Message:"
-				}
-			]
-		}
-	]);
-
-	callback(false, rewards);
-};
-
 
 module.exports = plugin;
